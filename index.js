c0e83325188e47791026c6d986bed15760f76218
Justin Hwang <jstnhwang@gmail.com>
2021-06-04 11:50:55 +0100

    Add user model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 const express = require("express");
 const app = express();
 
@@ -27,6 +28,7 @@ app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 // PORT
 const port = process.env.PORT || 3000;
---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const { User, validate } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {});
+
+module.exports = router;
