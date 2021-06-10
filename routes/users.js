21acb3ad0143d8534b344fed462bfe868cb897b9
Justin Hwang <jstnhwang@gmail.com>
2021-06-11 00:15:16 +0100

    Add precise user get method after auth

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 const { User, validate } = require("../models/user");
+const auth = require("../middleware/auth");
 const _ = require("lodash");
 const bycrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
