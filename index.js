5c62a94abfe1ae466ed45a570a5c4cd11eff8f03
Justin Hwang <jstnhwang@gmail.com>
2021-06-10 22:16:05 +0100

    Change URI to connect with  MongoDB Atlas

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
+const config = require("config");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
+
 try {
   mongoose.connect(
-    "mongodb://localhost/vidly",
+    "mongodb+srv://jstnhwang:QAYaYFtwg2UQVIdJ@cluster0.rvnwj.mongodb.net/vidly?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -29,6 +36,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 // PORT
 const port = process.env.PORT || 3000;
