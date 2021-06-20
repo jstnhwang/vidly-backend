827a4029f6cd3a1d83beaf4959f905d014e564ab
Justin Hwang <jstnhwang@gmail.com>
2021-06-20 22:01:06 +0100

    Change folder name from startup to start

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const winston = require("winston");
 const express = require("express");
 const app = express();
 
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
+require("./start/logging")();
+require("./start/routes")(app);
+require("./start/db")();
+require("./start/config")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
