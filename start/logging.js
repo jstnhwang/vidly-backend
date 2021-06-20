06fc185bf76de867233e47f62ac947715502623f
Justin Hwang <jstnhwang@gmail.com>
2021-06-20 22:21:03 +0100

    Change winston the way winston handle exceptions

--- start/logging.js
+++ start/logging.js
@@ -2,14 +2,26 @@ const winston = require("winston");
 require("express-async-errors");
 
 module.exports = function () {
-  winston.exceptions.handle()(
-    new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    new winston.transports.File({ filename: "uncaughtExceptions.log " })
-  );
+  const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: { service: "user-service" },
+    transports: [
+      new winston.transports.Console({ colorize: true, prettyPrint: true }),
+      new winston.transports.File({ filename: "error.log", level: "error", handleExceptions: true })
+    ],
+    exitOnError: true
+  });
+
+  if (process.env.NODE_ENV !== "production") {
+    logger.add(
+      new winston.transports.Console({
+        format: winston.format.simple()
+      })
+    );
+  }
 
   process.on("unhandledRejection", ex => {
     throw ex;
   });
-
-  winston.add(new winston.transports.File({ filename: "logfile.log " }));
 };
